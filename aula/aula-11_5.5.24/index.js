95c912f12c225b0cbcd71f6d1dda4db3babd4039
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 09:54:44 -0300

    aula 11/12

--- aula/aula-11_5.5.24/index.js
+++ aula/aula-11_5.5.24/index.js
@@ -1,13 +1,34 @@
+// parte 1 - requisicao que se conecta noa API do chatGPT
+
+// import OpenAI from "openai";
+
+//     const openai = new OpenAI({
+//         organization: "org-sWkbP85KoTzR03UrcxtFsRfm",
+//         apiKey: "vsk-proj-ceoHGD4z1S6yZuDhiwknT3BlbkFJtRbEPT5qZQ8I3n4RU8cO",
+//     })
+
+//     const chatCompletion = await openai.chat.completions.create({
+//         model: 'gpt-3.5-turbo',
+//         messages: [{'role': 'user', 'content': 'quem e voce?' }]
+//     })
+
+//     console.log(chatCompletion.choices[0].message)
+
+// parte 2 - cria o backend completo com api local
+
 import OpenAI from "openai";
+import express from 'express';
+import bodyParser from "body-parser";
+import cors from 'cors'
 
-const openai = new OpenAI({
-    organization: "",
-    apiKey: "",
-})
+    const openai = new OpenAI({
+        organization: "org-sWkbP85KoTzR03UrcxtFsRfm",
+        apiKey: "vsk-proj-ceoHGD4z1S6yZuDhiwknT3BlbkFJtRbEPT5qZQ8I3n4RU8cO",
+    })
 
-const chatCompletion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [{'role': 'user', 'content': 'quem e voce?' }]
-})
+    const chatCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{'role': 'user', 'content': 'quem e voce?' }]
+    })
 
-console.log(chatCompletion.choices[0].message)
+    console.log(chatCompletion.choices[0].message)
